27a47f706c9208b2c4f8b40292344c1e7467015c
Dave Beck <dacb@uw.edu>
2018-07-25 08:59:15 -0700

    replaced csv output with tsv

--- P1_TableGenerator.py
+++ P1_TableGenerator.py
@@ -19,7 +19,7 @@ for root, dirs, files in os.walk(homedir):
 
 current_file = 0
 f = open("seq_dic.csv", "w")
-f.write("organism,locus,product_id,product,translation\n")
+f.write("organism\tlocus\tproduct_id\tproduct\ttranslation\n")
 print("Mission start:")
 for root, dirs, files in os.walk(homedir):
     for file in files:
@@ -33,7 +33,7 @@ for root, dirs, files in os.walk(homedir):
                     organism = seq_record.annotations["organism"]
                     for feature in seq_record.features:
                         if feature.type == 'CDS':
-                            f.write(f"{organism},{seq_record.id},{feature.qualifiers['product'][0] if 'product' in feature.qualifiers else np.nan},{feature.qualifiers['protein_id'][0] if 'protein_id' in feature.qualifiers else np.nan},{feature.qualifiers['translation'][0] if 'translation' in feature.qualifiers else np.nan}\n")
+                            f.write(f"{organism}\t{seq_record.id}\t{feature.qualifiers['product'][0] if 'product' in feature.qualifiers else np.nan}\t{feature.qualifiers['protein_id'][0] if 'protein_id' in feature.qualifiers else np.nan}\t{feature.qualifiers['translation'][0] if 'translation' in feature.qualifiers else np.nan}\n")
                             # commented out the line below to speed up code
                             # f.flush() 
             except AttributeError:
