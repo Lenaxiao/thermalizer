45ef9fe3d4104880036cf173b1ff48dd223390ff
lenaxiao <lenaxiao@uw.edu>
2018-08-06 16:18:02 -0700

    find the most significant E-value for every protein

--- P4_hmmscan_Parser.py
+++ P4_hmmscan_Parser.py
@@ -8,11 +8,23 @@ import pandas as pd
 
 hmmin = "Pfam_table.txt"
 
+# find the most significant E-value for every protein.
+f = open("HitResult.csv", "w")
+f.write("product_id\tEvalue\n")
+print("Progressing...")
+wrong = 0
 for qresult in SearchIO.parse(hmmin, 'hmmer3-tab'):
+	wong = wrong + 1
+    top = 0.01  # Evalue larger than 0.01 is not statistally significant.
     for item in qresult.hits:
-        print(item)
-        print(item.evalue) # this will print the evalue of each domain hit
+        val = item.evalue
+        if val < top:
+            top = val
+        else:
+            pass
+    f.write(f"{qresult.id}\t{top}\n")
 
+f.close()
 stop_time = datetime.now()
 total_time = stop_time - start_time
 print("run time was:", total_time)
